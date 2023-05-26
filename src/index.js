e384e9c406d0085084664adb46d021c8364875a2
SandraKoch <sandraannakoch@gmail.com>
2023-05-26 22:06:50 +0200

    searchImages function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
 
-const axios = require('axios');
+const searchFormEl = document.querySelector('#search-form');
+const inputEl = document.querySelector('[name="searchQuery"]');
+const imagesContainerEl = document.querySelector('.images-container');
 
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
+function handleResults(results) {
+  imagesContainerEl.innerHTML = '';
+  if (results === []) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  console.log('displayImg');
+  //   displayImages(results);
+}
+
+function displayImages(results) {
+  for (result of results) {
+  }
+}
+async function searchImages(query) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=36788203-2c78e2a924ca1cc7e222b7ed9&image_type=photo&orientation=horizontal&q=${query}&safesearch=true&per_page=20&page=1`
+    );
+    console.log(response);
+    const imagesArray = response.data.hits;
+    console.log(imagesArray);
+    const mappedArr = imagesArray.map(image => {
+      return {
+        webformatURL: image.webformatURL,
+        largeImageURL: image.largeImageURL,
+        tags: image.tags,
+        likes: image.likes,
+        views: image.views,
+        comments: image.comments,
+        downloads: image.downloads,
+      };
+    });
+    return mappedArr;
+  } catch (error) {
+    console.log('Error in try ... catch', error.toString());
+  }
+}
+
+searchFormEl.addEventListener('submit', async e => {
+  e.preventDefault();
+  const foundImages = await searchImages(inputEl.value);
+  console.log('foundImages', foundImages);
+  handleResults(foundImages);
+  //   displayImages();
+});
