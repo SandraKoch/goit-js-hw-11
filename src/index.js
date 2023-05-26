ee2f0a6abc1aad1e4bcf31fd461663073004f136
SandraKoch <sandraannakoch@gmail.com>
2023-05-26 19:35:45 +0200

    notiflix, axios and isomnia setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const axios = require('axios');
+
+// axios.get('/user?ID=12345')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
